99a7bc767da760931e1a46a492524e0a46353906
AayushKP <ayushprokashyap@gmail.com>
2026-06-28 16:39:47 +0530

    add monthly summary

--- python/expense-tracker/analytics.py
+++ python/expense-tracker/analytics.py
@@ -6,3 +6,12 @@ def category_summary(expenses: list[dict]) -> dict:
         summary[category] = amount + summary.get(category, 0)
 
     return summary
+
+
+def monthly_summary(expenses: list[dict]) -> dict:
+    summary = {}
+    for expense in expenses:
+        date = expense["date"]
+        amount = expense.get("amount", 0)
+        summary[date] = amount + summary.get(date, 0)
+    return summary
