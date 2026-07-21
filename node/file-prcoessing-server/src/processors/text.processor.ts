0004189a58a565b2be8216e806daac91d7fe300d
AayushKP <ayushprokashyap@gmail.com>
2026-07-21 19:56:08 +0530

    complete upload flow

---
+++ node/file-prcoessing-server/src/processors/text.processor.ts
@@ -0,0 +1,42 @@
+import fs from "node:fs";
+import readline from "node:readline";
+
+export interface FileStats {
+  lines: number;
+  words: number;
+  characters: number;
+}
+
+export const processTextFile = async (filePath: string): Promise<FileStats> => {
+  // TODO:
+  // create a readable stream
+  const fileStream = fs.createReadStream(filePath, {
+    encoding: "utf8",
+  });
+  // TODO:
+  // create a readline interface
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelat: Infinity,
+  });
+
+  let lines = 0;
+  let words = 0;
+  let characters = 0;
+
+  for await (const line of rl) {
+    lines++;
+
+    const trimmed = line.trim();
+    if (trimmed.length > 0) {
+      words += trimmed.split(/\s+/).length;
+    }
+    characters += line.length;
+  }
+
+  return {
+    lines,
+    words,
+    characters,
+  };
+};
