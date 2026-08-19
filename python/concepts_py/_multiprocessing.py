75cc67b13c2c70f6d386b993b4e42fec243744db
AayushKP <ayushprokashyap@gmail.com>
2026-08-19 13:03:59 +0530

    threading_&_multiproc

---
+++ python/concepts_py/_multiprocessing.py
@@ -0,0 +1,24 @@
+import time
+from multiprocessing import Process
+
+
+def brew_coffee(name):
+    print(f"{name} coffee brewing")
+    time.sleep(3)
+    print(f"End of {name} coffee brewing")
+
+
+if __name__ == "__main__":
+    coffee_makers = [
+        Process(target=brew_coffee, args=(f"Coffee Maker #{i + 1}",)) for i in range(3)
+    ]
+
+    # Start all process
+    for p in coffee_makers:
+        p.start()
+
+    # wait for all to complete
+    for p in coffee_makers:
+        p.join()
+
+    print("All coffee served")
