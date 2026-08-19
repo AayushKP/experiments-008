cbe023efd5de525439d5f9813edf886f5ec215e5
AayushKP <ayushprokashyap@gmail.com>
2026-08-19 13:01:35 +0530

    Create _threading.py

---
+++ python/threading_python/_threading.py
@@ -0,0 +1,26 @@
+import threading
+import time
+
+
+def take_orders():
+    for i in range(1, 4):
+        print(f"Taking order for #{i}")
+        time.sleep(2)
+
+
+def brew_coffee():
+    for i in range(1, 4):
+        print(f"Brewing coffee for #{i}")
+        time.sleep(3)
+
+
+# create threads
+order_thread = threading.Thread(target=take_orders)
+brewing_thread = threading.Thread(target=brew_coffee)
+
+order_thread.start()
+brewing_thread.start()
+
+# wait for both to finish
+order_thread.join()
+brewing_thread.join()
